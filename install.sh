3bacb6a3a398db2a08041aa47b8eadc32b7f6cad
Alex Holzbach <ajholzbach>
2024-01-03 11:50:28 -0600

    Fix installation script for Oh My Zsh plugins

--- install.sh
+++ install.sh
@@ -86,13 +86,11 @@ fi
 echo "Installing Oh My Zsh plugins..."
 if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-fi
 else
     echo "zsh-syntax-highlighting plugin already installed."
 fi
 if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
 else
     echo "zsh-autosuggestions plugin already installed."
 fi
