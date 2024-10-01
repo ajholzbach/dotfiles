8bff09f7a03278d09b7074096451d0840c15cc42
alexholzbach <alexander.holzbach@doordash.com>
2024-10-01 10:25:25 -0400

    modified dedupe path script to be python

--- scripts/dedupe_path.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-export PATH=$(echo -n $PATH | awk -v RS=: '!($0 in a) {a[$0]; printf("%s%s", length(a) > 1 ? ":" : "", $0)}')
---
+++ scripts/deduped_path.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# run the following command to fix PATH variable
+# export PATH=$(./deduped_path.py)
+import os
+def main():
+    # Get the current PATH environment variable
+    path = os.environ.get('PATH', '')
+
+    # Split the PATH into a list of directories
+    path_list = path.split(':')
+
+    # Remove duplicates while preserving order
+    unique_path_list = sorted(set(path_list), key=path_list.index)
+
+    # Join the list back into a single string
+    new_path = ':'.join(unique_path_list)
+
+    # Print the new PATH
+    print(new_path)
+
+if __name__ == "__main__":
+    main()
+
