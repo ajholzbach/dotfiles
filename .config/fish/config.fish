f1e62e8b25ab99fe52b1b5378a912032bf777384
Alex Holzbach <alex.holzbach@doordash.com>
2025-07-01 10:34:13 -0400

    Updated config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,12 @@
 if status is-interactive
-    starship init fish | source
-    enable_transience
+    # starship.rs prompt
+    if type -q starship
+        # starship init fish | source
+        echo "source (starship init fish --print-full-init | psub)" | source
+        enable_transience
+    end
+
+    # zoxide
     if type -q zoxide
         zoxide init --cmd cd fish | source
     end
