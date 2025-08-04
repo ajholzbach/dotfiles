09c2f46b2558bea84b39b160f103a3b252245c61
Alex Holzbach <ajholzbach>
2025-08-04 15:40:18 +0800

    Updated fisher_setup

--- .config/fish/functions/fisher_setup.fish
+++ .config/fish/functions/fisher_setup.fish
@@ -1,6 +1,6 @@
 function fisher_setup --description 'Bootstrap Fisher and plugins'
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
     if test -f "$XDG_CONFIG_HOME/fish/themes/Catppuccin Mocha.theme"
-        fish_config theme save "Catppuccin Mocha"
+        echo y | fish_config theme save "Catppuccin Mocha" > /dev/null
     end
 end
